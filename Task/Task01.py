4dd1931a2e38b55163511608e16dec939e735c7b
swapy <swapy1525@gmai.com>
2024-06-13 18:24:27 +0530

    Added Task

---
+++ Task/Task01.py
@@ -0,0 +1,27 @@
+# Task 1
+
+# 1
+# = is a assignment operator and it will assign the value to a variable
+#eg. x=1000  here x is a variable and 1000 is the value assigned to the variable.
+
+# == is a equality operator and it will compare the values. and verify if the values are same or not.
+print(5==5)
+# it will always the value in boolean. True or false.
+
+# 2
+# ** is a power math function. It is a exponential operator.
+# print(2**2)   2 to power 2.
+print(2**2)
+
+# 3
+# ^ is Bitwise XOR operator (gate)
+#whenever there are same values in 0 & 1 it will give 0 and when the values are different it will give value as 1
+
+
+
+
+
+
+
+
+
---
+++ Task/Task02.py
@@ -0,0 +1,20 @@
+#Task 2
+
+#Square Cube
+
+Number=float(input("Enter the number\n"))
+
+print("The square of number is", Number**2)
+print("The cube of the number is", Number**3)
+
+
+# Create a program that takes two numbers as input and prints whether the first number is greater than, less than,
+#or equal to the second number.
+
+Num1=float(input("Number 1 is"))
+Num2=float(input("Number 2 is"))
+
+
+print(f" {Num1} {'greater than' if Num1 > Num2 else 'is less than' if Num2 > Num1 else 'is equal to '} {Num2}")12
+
+
